1df8821f9e01ae0b40720df3385fed059d6b7011
Willams Sousa <osdeving@gmail.com>
2025-10-02 14:54:32 +0000

    Oct 2, 2025, 11:54 AM

---
+++ src/servers/tools.py
@@ -0,0 +1,26 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP()
+
+@mcp.tool(
+    # se não fornecido usa o nome da função
+    name='find_products',
+
+    # descrição da tool, se fornecido aqui não usará docstring para esse fim.
+    description='Search the product catalog with optional category filtering.',
+
+    # conjunto de tags da tool, pode ser usado pelo server e alguns casos pelo client também
+    tags={'catalog', 'search'},
+
+    # 
+    meta={'version': '1.2', 'author': 'product-team'}
+)
+def search_products(query: str, category: str | None = None) -> list[dict]:
+    """Internal description (ignored if description is provided above)."""
+    print(f'Searching for "${query}" in "${category}"')
+    return [{"id": 2, "name": "Wireless Keyboard"}]
+
+
+
+
+
